477107dbe6c118252aa6f37a1e8b95eea30f37e5
Siva kumar <99171876+Siva1kumar@users.noreply.github.com>
2026-06-21 20:03:23 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,22 +1,13 @@
-// firebase-config.js
-// Replace the following configuration object with your actual Firebase project
-// settings from the Firebase console. See https://firebase.google.com/docs/web/setup
-// for details on how to obtain these values.
-
-// Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBTInhJBnA4i5YONwcv-hnKj_JIZEOHPy0",
+  authDomain: "sk-tech-c4b11.firebaseapp.com",
+  projectId: "sk-tech-c4b11",
+  storageBucket: "sk-tech-c4b11.firebasestorage.app",
+  messagingSenderId: "1028806558698",
+  appId: "1:1028806558698:web:9440bfda22f89d93122893"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Initialize Firestore and Auth services. These are used in auth.js and other
-// scripts to access your Firestore database and authentication features.
-const db = firebase.firestore();
 const auth = firebase.auth();
+const db = firebase.firestore();
